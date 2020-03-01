56a613a2e7b43f5fe62798e266d5b12f6f6eb5bd
ignitexr <deb@ignitexr.com>
2020-02-29 17:31:05 -0700

    add landscape check

--- aframe-components/game.js
+++ aframe-components/game.js
@@ -3,10 +3,25 @@
 AFRAME.registerComponent('initialize-game', {
   dependencies: ['bounds'],
   init() {  
-      // get the camera bounds component for positioning
-      var bounds = document.querySelector('[bounds]').components.bounds;
-      this.bounds = bounds; 
-      this.generate_grid()
+    // get the camera bounds component for positioning
+    var bounds = document.querySelector('[bounds]').components.bounds;
+    this.bounds = bounds; 
+    this.generate_grid()    
+
+    XR.addCameraPipelineModule({
+      name: 'screenorientation',
+      onDeviceOrientationChange: ({ GLctx, videoWidth, videoHeight, orientation }) => {
+        const rotateScreen = document.getElementById('rotateScreen')
+        if (orientation != 0) {
+          rotateScreen.style.visibility = 'visible';
+        }
+        else {
+          rotateScreen.style.visibility = 'hidden';
+        }
+      }
+    })
+
+    
   },    
   
 
@@ -17,6 +32,7 @@ AFRAME.registerComponent('initialize-game', {
 
     var pos = new THREE.Vector3()
     pos = this.bounds.getTopLeft(zPos)
+    // to position object at edges, multiply by scale
     var scale = .5
     pos.x += .5 * scale
     pos.y -= .5 * scale   
